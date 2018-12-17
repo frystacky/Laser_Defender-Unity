28f81cb819b41b48f814bd1d8ed949eb148448e8
Daniel Frystacky <danielfrystacky@gmail.com>
2018-12-17 15:10:31 -0500

    Player is now abled to shot laser and laser object gets deleted off screen

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 
-    //config var's
-    [SerializeField] float moveSpeed = 10f;                                                  //changes move speed of player
-    [SerializeField] float padding = 1f;                                                     //padding so player doesnt go half off the screen
+    //config parma's
+    [SerializeField] float moveSpeed = 10f;                          //changes move speed of player
+    [SerializeField] float padding = 1f;                             //padding so player doesnt go half off the screen
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
 
     float xMin;
     float xMax;
@@ -22,6 +27,49 @@ public class Player : MonoBehaviour {
 
 	}
 
+
+    // Update is called once per frame
+    void Update () {
+        Move();
+        Fire();
+	}
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        { 
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;          //added Time.deltaTime to get frame rate independent
+        //Debug.Log(deltaX);                                                            //for debuging to see frame rate value in console
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);           // Mathf.Clamp will limit our movement to camera x max and x min
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+
+    }
+
     private void SetUpMoveBounderies()
     {
         Camera gameCamera = Camera.main;
@@ -31,24 +79,5 @@ public class Player : MonoBehaviour {
         yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
         yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
-
-    // Update is called once per frame
-    void Update () {
-        Move();
-	}
-
-    private void Move()
-    {
-        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;              //added Time.deltaTime to get frame rate independent
-        //Debug.Log(deltaX);                                                                //for debuging to see frame rate value in console
-        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
-        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);                // Mathf.Clamp will limit our movement to camera x max and x min
-        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
-
-        transform.position = new Vector2(newXPos, newYPos);
-
-    }
-
 
 }
